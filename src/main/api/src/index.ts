ddfb41faea657f904beb0344cfc7c9da0221861f
Rich Vaughan <richard.vaughan16@btinternet.com>
2022-03-30 03:04:15 +0100

    adding db query to calculate a user's concurrent streams

--- src/main/api/src/index.ts
+++ src/main/api/src/index.ts
@@ -1,6 +1,6 @@
 import {APIGatewayEvent, APIGatewayProxyResult} from 'aws-lambda';
 import {generateResponse} from './response-generator';
-import {putUserStreamItem} from './userStreamRepository';
+import {putUserStreamItem, getConcurrentStreams} from './userStreamRepository';
 
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
     try {
@@ -21,35 +21,38 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
 const processRequest = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
 
     const user = event.pathParameters?.userId;
-    let concurrentStreams = 1;
-
-    if (user) {
-        if (concurrentStreams <=3 ) {
-            
-            try {
-                await putUserStreamItem(user);
-                concurrentStreams++
-            } catch (error) {
-                const errorMessage = `DB put error: ${error.toString()}`;
-                throw new Error(errorMessage);
-            }
-            
-            return generateResponse(event,
-                200,
-                JSON.stringify({
-                    user: user,
-                    concurrentStreams: concurrentStreams
-                }),
-                {
-                    'content-type': 'application/json'
-                });
-        }
-        else {
-            return generateResponse(event, 400, 'User ' + user + ' is trying to watch ' + concurrentStreams + ' concurrent streams, which is too many.');
-        }
-    }
-    else {
+    if (!user) {
         return generateResponse(event, 400, 'Please specify a userId');
     }
 
+    let concurrentStreams: number;
+    try {
+        concurrentStreams = await getConcurrentStreams(user);
+    } catch (error) {
+        const errorMessage = `DB query error: ${error.toString()}`;
+        throw new Error(errorMessage);
+    }
+
+    if (concurrentStreams <=2 ) {
+        try {
+            await putUserStreamItem(user);
+            concurrentStreams++
+        } catch (error) {
+            const errorMessage = `DB put error: ${error.toString()}`;
+            throw new Error(errorMessage);
+        }
+        
+        return generateResponse(event,
+            200,
+            JSON.stringify({
+                user: user,
+                concurrentStreams: concurrentStreams
+            }),
+            {
+                'content-type': 'application/json'
+            });
+    }
+    else {
+        return generateResponse(event, 400, 'User ' + user + ' is trying to watch ' + (concurrentStreams+1) + ' concurrent streams, which is too many.');
+    }
 }
--- src/main/api/src/userStreamRepository.ts
+++ src/main/api/src/userStreamRepository.ts
@@ -1,4 +1,4 @@
-import DynamoDB, {Put} from 'aws-sdk/clients/dynamodb'
+import DynamoDB from 'aws-sdk/clients/dynamodb'
 
 export const dynamoDB = new DynamoDB({apiVersion: '2012-08-10'});
 export const putUserStreamItem = async (userId: String): Promise<void> => {
@@ -15,6 +15,30 @@ export const putUserStreamItem = async (userId: String): Promise<void> => {
             .promise();
     } catch (error) {
         console.log('Error putting user stream item', error)
+        throw new Error(error);
     }
     console.log('User stream item inserted: ', userStreamItem);
+}
+
+export const getConcurrentStreams = async (userId: String): Promise<number> => {
+    const params: DynamoDB.QueryInput = {
+        TableName : "streaming-table",
+        KeyConditionExpression: "#pk = :pk",
+        ExpressionAttributeNames: {
+            '#pk': 'pk'
+        },
+        ExpressionAttributeValues: {
+            ':pk': {
+                S: 'userId#' + userId
+            }
+        }
+    };
+    try {
+        const userStreamItems = await dynamoDB.query(params).promise();
+        console.log('User stream items', userStreamItems);
+        return userStreamItems.Count ?? 0;
+    } catch (error) {
+        console.log('Error putting user stream item', error)
+        throw new Error(error);
+    }
 }
